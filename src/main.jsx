bbe7c6154f66d982ca267165f9079922bf10304b
maximo cavero <cavero.maximo.l@gmail.com>
2023-11-08 20:01:58 -0300

    home terminado

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import PokeProvider from "./context/pokeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <PokeProvider>
       <App />
-    </PokeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,26 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { PokeContext } from '../context/pokeContext';
+import React, { useState, useEffect } from 'react';
 
 export const Home = () => {
   const PokeApi = "https://pokeapi.co/api/v2/pokemon/pikachu";
   const [data, setData] = useState([]);
- 
-  const photo = data.sprites.other.dream_world.front_default;
+
+  const getPokemon = async () => {
+    const res = await fetch(PokeApi);
+    const datos = await res.json();
+    const img = datos.sprites.other.dream_world.front_default;
+    setData(img);
+  };
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <>
     <div className="text-center">
     <h1>Bienvenido maestro Pokémon</h1>
       {
         <img
-          src={`${photo}`}
+          src={`${data}`}
           alt="Pikachu"
           width="300"
         />
